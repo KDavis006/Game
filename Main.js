34314d20d6b8cfc626efa23e99da2eb44b795e28
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2022-12-07 15:21:34 -0700

    save

--- Main.js
+++ Main.js
@@ -1,9 +1,33 @@
+let rand1 = 0
+let rand2 = 0
+let rand3 = 0
+let rand4 = 0
+
+for(i=0;i<=99;i++){
+ rand1 = Math.round(Math.random()*25)
+ rand2 = Math.round(Math.random()*25)
+ rand3 = Math.round(Math.random()*25)
+ rand4 = Math.round(Math.random()*25)
+ if(rand1 == rand2 || rand1 == rand3 || rand1 == rand4 || rand2 == rand3 || rand2 == rand4 || rand3 == rand4){
+  continue
+ } else {
+  return
+ }
+}
+
+for(let i = 1; i <= 4; i++){
+    let kbbutton = Math.round(Math.random()*26);
+    let z = document.getElementById(`button${kbbutton}`).innerText
+    let x = `rand${i}`;
+    document.getElementById(`character${x}`).innerText = z;
+}
+
 function fill(character){
  for(let i = 1; i <= 25; i++){
  let x = document.getElementById(`character${i}`).innerText
  if(x == ""){
   
-  document.getElementById(`character${i}`).innerText = toUpperCase(character)
+  document.getElementById(`character${i}`).innerText = character
   console.log('This is working fine!');
   return
  }

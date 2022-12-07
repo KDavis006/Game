f5d46ad01f1a436da9289d6abeb83cf5dd0c5398
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2022-12-07 13:00:22 -0700

    save

--- Main.js
+++ Main.js
@@ -1,8 +1,10 @@
-function fill(char){
+function fill(character){
  for(let i = 1; i <= 25; i++){
- let x = document.getElementById(  `${i}`).innerHTML
- if(x = ""){
-  x = char;
+ let x = document.getElementById(`character${i}`).innerText
+ if(x == ""){
+  
+  document.getElementById(`character${i}`).innerText = toUpperCase(character)
+  console.log('This is working fine!');
   return
  }
  }
